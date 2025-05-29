964b6f5a6c7b14d5d7a89d30146ef1c79595b53d
309 <jing309@gmail.com>
2025-05-29 18:21:11 +0900

    Restore Header component with Instagram and Threads buttons

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import { Header } from './Header';
 import { Navigation } from '../ui/Navigation';
 
 interface LayoutProps {
@@ -13,7 +14,8 @@ export function Layout({ children, currentPage, onPageChange }: LayoutProps) {
     <div className={`w-full min-h-screen font-inter flex flex-col ${
       isCalendarPage ? 'bg-white' : 'bg-dark-navy'
     }`}>
-      <main className="flex-1 flex flex-col pt-4 pb-24">
+      <Header />
+      <main className="flex-1 flex flex-col pt-16 pb-24">
         {children}
       </main>
       <Navigation activeTab={currentPage} onTabChange={onPageChange} />
